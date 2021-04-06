9db62ab44436344babc5c95b1093b62ca864e354
agustina <santarelliagustina98@gmail.com>
2021-04-06 18:39:35 -0300

    Project Server

---
+++ README.md
@@ -0,0 +1,50 @@
+# Laboratorio 2: Aplicación Servidor
+
+## Modo de Trabajo
+Para realizar el proyecto trabajamos mayormente en sesiones interactivas utilizando el paquete *teletype* de Atom y comunicandonos por Discord, 
+aunque tambien hubo aportes individuales pero siempre con el conocimiento de todo el grupo.
+
+## Protocolo HFTP
+Home-made File Transfer Protocol (HFTP) es un protocolo de capa de aplicación que utiliza TCP como protocolo de transporte. TCP garantiza una entrega segura, libre de errores y en orden de todas las transacciones hechas con HFTP
+
+### Estructura del Servidor
+#### Ejercicio Estrella
+
+Para poder manejar varias conexiones, pensamos en simultaneidad y nos llevo apensar en concurrencia o threads. Luego nos dimos cuenta de que lo unico que queriamos es que para cada conexion entrante, el servidor aceptara el socket y no "se confundiera" al enviar y recibir informacion.
+
+La técnica que permite que una aplicación ejecute simultáneamente varias operaciones en el mismo espacio de proceso se llama Threading. A cada flujo de ejecución que se origina durante el procesamiento se le denomina hilo o subproceso, pudiendo realizar o no una misma tarea.
+Ésta estructura utiliza ``threads``, módulo de Python el cual hace posible la programación con hilos, y que nos posibilitó implementar la conexión de múltiples cliente al server.
+Cada vez que un cliente levanta el server, se intancia un nuevo thread, y llama a la función
+```python
+def threaded(connection):
+```
+donde dentro de la misma se va a encontrar nuestra función ```handle``` que va a manejar las diferentes solicitudes del cliente hasta que el mismo envíe el comando ``quit``, el cual procederá a su desconexión, ya que se instanciará la variable ``connected``en ``False``
+
+### Decisiones de diseño
+Algunas de las decisiones de diseño fueron:
+
+* En ``connection.py`` la función ``handle`` va a ser la encargada de manejar lo que ocurra en consola, tanto sean el manejo de errores como el manejo de los comandos. Para esto último, en ``handle`` tenemos una función llamada ``ok_command`` la cual va a verificar que lo que el cliente está pasando es una comando válido y ``ok_arguments`` que verifica los argumentos del comando, de ser así instantaneamente se llama a la función ``do_command``, la cual está dividida por casos según cual haya sido el comando solicitado.
+* Manejar las distintas conexiones por ``threads`` como hemos mencionado en la estructura del servidor.
+* Hemos creado para el objeto ``Connection`` un atributo ``error_count`` que cuenta la cantidad de ocurrencia de errores de tipo no fatales y en la funcion ``handle`` verificamos que ``error_count < MAX_ERRORS`` donde ``MAX_ERRORS`` es una constante agregada en ``constants.py``. Al alcanzarlo devuelve error 101.
+* En la funcion ``handle`` cuando creamos nuestro buffer decodificando la data que nos llega, tenemos en cuenta que sea un caracter de tipo ascii, pues si no lo es, devolvemos error ``101`` y cerramos la conexión.
+
+
+
+
+
+### Preguntas
+#### ¿Qué estrategias existen para poder implementar este mismo servidor pero con capacidad de atender múltiples clientes simultáneamente?
+Un programa multiproceso contiene dos o más partes que pueden ejecutarse simultáneamente. Cada parte de dicho programa se denomina hilo, y cada hilo define una ruta de ejecución separada. La programación de sockets multiproceso describe que un servidor de sockets multiproceso puede comunicarse con más de un cliente al mismo tiempo en la misma red.
+Como el server inicialmente sólo podía atender a un cliente a la vez, la estrategia que investigamos y que hemos implementado, como anteriormente mencionamos, es la creación de hilos multihilo o multithreaded para lograr la conexión simultánea de varios clientes. El código hace uso del módulo estándar threading, que otorga diversas funciones, como por ejemplo la que hemos utilizado:
+```python
+start_new_thread(threaded, (connection,)):
+```
+que comienza un nuevo hilo tomando la funcion ``threaded`` y los parametros que tomaría la misma.
+
+
+## Autores
+
+* **Benitez Iván** - 
+* **Santarelli Agustina** - 
+* **Vilar Dávila Valentín** -  
+
---
+++ connection.py
@@ -0,0 +1,270 @@
+# encoding: utf-8
+# Revisión 2019 (a Python 3 y base64): Pablo Ventura
+# Copyright 2014 Carlos Bederián
+# $Id: connection.py 455 2011-05-01 00:32:09Z carlos $
+
+import socket
+from constants import *
+from base64 import b64encode
+import os
+
+
+class Connection(object):
+    """
+    Conexión punto a punto entre el servidor y un cliente.
+    Se encarga de satisfacer los pedidos del cliente hasta
+    que termina la conexión.
+    """
+
+    def __init__(self, socket, directory):
+
+        self.conn, self.client = socket.accept()
+        self.dir = directory
+        self.error_count = 0
+
+    def file_listing(self):
+        try:
+            entries = os.listdir(str(self.dir))
+            msg = str(CODE_OK) + " " + str(error_messages[CODE_OK]) + EOL
+            for e in entries:
+                msg += (str(e) + EOL)
+            msg += EOL
+
+        except BaseException:
+            msg = str(INTERNAL_ERROR) + " " + \
+                str(error_messages[INTERNAL_ERROR]) + EOL
+
+        return msg
+
+    def get_size(self, file):
+
+        filename = self.dir + '/' + file
+
+        try:
+            if len(file) < 256:
+                sizeoffile = os.stat(filename).st_size
+                msg = str(CODE_OK) + " " + str(error_messages[CODE_OK]) + EOL
+                msg += str(sizeoffile) + EOL
+            else:
+                msg = str(FILE_NOT_FOUND) + " " + \
+                    str(error_messages[FILE_NOT_FOUND]) + EOL
+                self.error_count += 1
+
+        except BaseException:
+            msg = str(FILE_NOT_FOUND) + " " + \
+                str(error_messages[FILE_NOT_FOUND]) + EOL
+            self.error_count += 1
+        return msg
+
+    def do_slice(self, file, begin, end):
+
+        filename = self.dir + '/' + file
+
+        try:
+
+            sizeoffile = os.stat(filename).st_size
+
+            if(int(begin) < sizeoffile and int(end) <= sizeoffile):
+                f = open(filename, "rb")
+                f.seek(begin)
+                message = f.read(end)
+                msg = b64encode(message) + (EOL.encode("ascii"))
+                f.close()
+            else:
+                msg = str(BAD_OFFSET) + " " + \
+                    str(error_messages[BAD_OFFSET]) + EOL
+                self.error_count += 1
+
+        except BaseException:
+            msg = str(INTERNAL_ERROR) + " " + \
+                str(error_messages[INTERNAL_ERROR]) + EOL
+
+        return msg
+
+    def quit(self):
+        return str(CODE_OK) + " " + str(error_messages[CODE_OK]) + EOL
+
+    def eol_incomplete(self, data):
+        if(len(data) > 1):
+            for i in range(0, len(data) - 1):
+                if data[i] is '\r' and data[i + 1] is not '\n':
+                    return True
+                elif data[i] is '\n' and data[i - 1] is not '\r':
+                    return True
+            return False
+        return False
+
+    def multiple_commands(self, data):
+        data_line = data.split(EOL)
+        return data_line
+
+    def ok_command(self, command):
+
+        if(command[0] in ['get_file_listing', 'get_metadata', 'get_slice', 'quit']):
+            return True
+        else:
+            return False
+
+    def ok_arguments(self, command):
+        if command[0] == 'get_file_listing':
+            if len(command) == 1:
+                return True
+            else:
+                return False
+        elif command[0] == 'get_metadata':
+            if (len(command) == 2):
+                return True
+            else:
+                return False
+        elif command[0] == 'get_slice':
+            if(len(command) == 4):
+                return True
+            else:
+                return False
+        elif command[0] == 'quit':
+            if len(command) == 1:
+                return True
+            else:
+                return False
+
+    def do_command(self, command):
+        connected = True
+        if(command[0] == 'get_file_listing'):
+            msg = self.file_listing()
+
+            self.conn.send(msg.encode("ascii"))
+            if(msg == str(INTERNAL_ERROR) + " " + str(error_messages[INTERNAL_ERROR]) + EOL):
+                self.conn.close()
+                connected = False
+
+        elif(command[0] == 'get_metadata'):
+
+            try:
+
+                # Debe venir el mensaje completo con codigo de error.
+                msg = self.get_size(command[1])
+
+                self.conn.send(msg.encode("ascii"))
+
+            except BaseException:
+                msg = str(INVALID_ARGUMENTS) + " " + \
+                    str(error_messages[INVALID_ARGUMENTS]) + EOL
+                self.error_count += 1
+                self.conn.send(msg.encode("ascii"))
+
+        elif(command[0] == 'get_slice'):
+
+            if len(command) < 4:
+                msg = self.quit()
+                self.conn.send(msg.encode('ascii'))
+                self.conn.close()
+                connected = False
+            else:
+                try:
+                    begin = int(command[2])
+                    size = int(command[3])
+                    msg = self.do_slice(command[1], begin, size)
+
+                    if(msg == str(INTERNAL_ERROR) + " " + str(error_messages[INTERNAL_ERROR]) + EOL):
+                        self.conn.send(msg.encode("ascii"))
+                        self.conn.close()
+                        connected = False
+                    else:
+                        self.conn.send(("0 OK" + EOL).encode("ascii"))
+                        self.conn.send(msg)
+
+                except ValueError:
+                    msg = str(INVALID_ARGUMENTS) + " " + \
+                        str(error_messages[INVALID_ARGUMENTS]) + EOL
+                    self.error_count += 1
+                    try:
+                        self.conn.send(msg.encode("ascii"))
+                    except BaseException:
+                        pass
+
+        elif(command[0] == 'quit'):
+            if (len(command) == 1):
+                msg = self.quit()
+                self.conn.send(msg.encode('ascii'))
+                self.conn.close()
+                connected = False
+            else:
+                msg = str(INVALID_ARGUMENTS) + " " + \
+                    str(error_messages[INVALID_ARGUMENTS]) + EOL
+                self.error_count += 1
+                self.conn.send(msg.encode("ascii"))
+
+        return connected
+
+    def handle(self):
+
+        connected = True
+        data_buffer = ""
+        while connected:
+            try:
+                data = self.conn.recv(BUFFER_SIZE)
+                if not data:
+                    self.conn.close()
+                    connected = False
+                    break
+                try:
+                    data_buffer += data.decode('ascii')
+                except BaseException:
+                    msg = str(BAD_REQUEST) + " " + \
+                        str(error_messages[BAD_REQUEST]) + EOL
+                    self.conn.send(msg.encode("ascii"))
+                    self.conn.close()
+
+                if self.error_count >= MAX_ERROR:
+                    msg = str(BAD_REQUEST) + " " + \
+                        str(error_messages[BAD_REQUEST]) + EOL
+                    self.conn.send(msg.encode("ascii"))
+                    self.conn.close()
+                    connected = False
+                    break
+
+                if EOL in data_buffer:
+                    if (self.eol_incomplete(data_buffer)):
+                        msg = str(BAD_EOL) + " " + \
+                            str(error_messages[BAD_EOL]) + EOL
+                        self.conn.send(msg.encode("ascii"))
+                        self.conn.close()
+                        connected = False
+                        break
+                    buf = self.multiple_commands(data_buffer)
+                    for i in range(0, len(buf)):
+
+                        data_parsed = buf[i].split()
+                        if(len(data_parsed) > 0):
+                            if(self.ok_command(data_parsed)):
+                                if(self.ok_arguments(data_parsed)):
+                                    if(i == len(buf)):
+                                        data_buffer = ""
+                                    connected = self.do_command(data_parsed)
+                                else:
+                                    msg = str(INVALID_ARGUMENTS) + " " + \
+                                        str(error_messages[INVALID_ARGUMENTS]) + EOL
+                                    self.error_count += 1
+                                    self.conn.send(msg.encode("ascii"))
+
+                            elif i == len(buf) and i > 1:
+                                resto = buf[i]
+                                data_buffer = resto
+                            else:
+                                msg = str(INVALID_COMMAND) + " " + \
+                                    str(error_messages[INVALID_COMMAND]) + EOL
+                                self.error_count += 1
+                                self.conn.send(msg.encode("ascii"))
+                        else:
+                            data_buffer = ""
+
+            except ConnectionResetError:
+                self.conn.close()
+                connected = False
+                break
+
+        return connected
+
+        """
+        Atiende eventos de la conexión hasta que termina.
+        """
